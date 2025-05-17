a6dcda28a29c8050a18f331879f1709d97554569
ahmedaidara05 <ahmedsaidaidara5@gmail.com>
2025-05-17 19:37:03 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,11 @@
-// Configuration et initialisation de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAljojXHODwHjStePWkhthWLRzrw3pUslQ",
-    authDomain: "la-voie-du-salut-36409.firebaseapp.com",
-    projectId: "la-voie-du-salut-36409",
-    storageBucket: "la-voie-du-salut-36409.firebasestorage.app",
-    messagingSenderId: "61439310820",
-    appId: "1:61439310820:web:52bfe8b862666ac13d25f1",
-    measurementId: "G-G9S1ST8K3R"
+    apiKey: "VOTRE_API_KEY",
+    authDomain: "VOTRE_AUTH_DOMAIN",
+    projectId: "VOTRE_PROJECT_ID",
+    storageBucket: "VOTRE_STORAGE_BUCKET",
+    messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+    appId: "VOTRE_APP_ID"
 };
 
-// Initialiser Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const db = firebase.firestore();
